5331e67341d0539fdbabcf92d482834268ab0e13
Igor Holanda <oigorholanda@gmail.com>
2022-10-17 22:00:14 -0300

    Mensagem WPP

--- script.js
+++ script.js
@@ -6,24 +6,25 @@ let tituloSobremesa;
 let precoSobremesa;
 let habilitado;
 
-function selecionarPrato (pratoClicado) {
+function selecionarPrato(pratoClicado) {
     // remover a classe selecionado do prato que foi marcado anteriormente
     const selected = document.querySelector('.pratos .selecionado');
 
     if (selected !== null) {
         selected.classList.remove('selecionado');
-    } 
+    }
     //Adicionar a classe selecionado
     pratoClicado.classList.add('selecionado');
-    //pegar o prato que foi clicado
+    //pegar a div do prato e do preço que foi clicado
     tituloPrato = pratoClicado.querySelector('.nome');
-    precoPrato = pratoClicado.querySelector('.preco');
+    precoPrato = pratoClicado.querySelector('.preco').innerHTML;
 
     console.log(tituloPrato);
-    habilitarBotao ();
+    console.log(precoPrato);
+    habilitarBotao();
 }
 
-function selecionarBebida (bebidaClicada) {
+function selecionarBebida(bebidaClicada) {
     // remover a classe selecionado da bebida que foi marcado anteriormente
     const selected = document.querySelector('.bebidas .selecionado');
 
@@ -32,15 +33,16 @@ function selecionarBebida (bebidaClicada) {
     }
     //Adicionar a classe selecionado
     bebidaClicada.classList.add('selecionado');
-    //pegar a bebida que foi clicada
+    //pegar a div da bebida e do preço que foi clicada
     tituloBebida = bebidaClicada.querySelector('.nome');
-    precoBebida = bebidaClicada.querySelector('.preco');
+    precoBebida = bebidaClicada.querySelector('.preco').innerHTML;
 
     console.log(tituloBebida);
-    habilitarBotao ();
+    console.log(precoBebida);
+    habilitarBotao();
 }
 
-function selecionarSobremesa (sobremesaClicada) {
+function selecionarSobremesa(sobremesaClicada) {
     // remover a classe selecionado da sobremesa que foi marcado anteriormente
     const selected = document.querySelector('.sobremesas .selecionado');
 
@@ -49,39 +51,46 @@ function selecionarSobremesa (sobremesaClicada) {
     }
     //Adicionar a classe selecionado
     sobremesaClicada.classList.add('selecionado');
-    //pegar a sobremesa que foi clicado
+    //pegar a div da sobremesa e do preço que foi clicado
     tituloSobremesa = sobremesaClicada.querySelector('.nome');
-    precoSobremesa = sobremesaClicada.querySelector('.preco');
+    precoSobremesa = sobremesaClicada.querySelector('.preco').innerHTML;
 
     console.log(tituloSobremesa);
-    habilitarBotao ();
+    console.log(precoSobremesa);
+    habilitarBotao();
 }
 
-function habilitarBotao () {
-    if (tituloPrato !== undefined && tituloBebida!== undefined && tituloSobremesa!== undefined) {
+function habilitarBotao() {
+    if (tituloPrato !== undefined && tituloBebida !== undefined && tituloSobremesa !== undefined) {
         const botao = document.querySelector('.botao');
         botao.classList.add('verde');
         botao.innerHTML = "Fechar pedido";
         habilitado = true;
-    } 
+    }
 }
 
-function enviarPedido () {
-    if (habilitado == null) {
-        console.log("Itens não selecionados");
+function enviarPedido() {
+    if (habilitado == undefined) {
+        window.alert("Selecione todos os itens primeiro");
     } else {
-    let precoFinal = (`${precoPrato.innerHTML} + 
-    ${precoBebida.innerHTML} + 
-    ${precoSobremesa.innerHTML}`)
-    
-    const msg = encodeURIComponent (`Olá, gostaria de fazer o pedido:
-    - Prato: ${tituloPrato.innerHTML}
-    - Bebida: ${tituloBebida.innerHTML}
-    - Sobremesa: ${tituloSobremesa.innerHTML}
-    Total: R$ ${precoFinal}
-    `)
+        precoPrato = precoPrato.replace('R$ ', '');
+        precoBebida = precoBebida.replace('R$ ', '');
+        precoSobremesa = precoSobremesa.replace('R$ ', '');
 
-    window.open ('https://wa.me/5561987654321?text='+ msg)
+        precoPrato = precoPrato.replace(',', '.');
+        precoBebida = precoBebida.replace(',', '.');
+        precoSobremesa = precoSobremesa.replace(',', '.');
+
+        let precoFinal = Number(precoPrato) + Number(precoBebida) + Number(precoSobremesa);
+
+        const msg = encodeURIComponent(`Olá, gostaria de fazer o pedido:\n
+        - Prato: ${tituloPrato.innerHTML}\n
+        - Bebida: ${tituloBebida.innerHTML}\n
+        - Sobremesa: ${tituloSobremesa.innerHTML}\n
+        Total: ${precoFinal.toFixed(2)}
+        `)
+
+        window.open('https://wa.me/5561987654321?text='+ msg)
     }
 }
 
